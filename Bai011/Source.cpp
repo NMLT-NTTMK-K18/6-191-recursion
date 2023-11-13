c14a8d922bde56d89f289c6d0af4e22831e85a2d
KevinNitroG <kevinnitro@duck.com>
2023-11-13 12:24:20 +0000

    💥 (Bai011): Done

--- Bai011/Source.cpp
+++ Bai011/Source.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include <limits>
+#include <iomanip>
+#include <cmath>
 using namespace std;
+
+int sum(int);
 
 int main()
 {
+    cout << "Problem 011 - To Vinh Tien - 22521474" << endl;
+    int n;
+    cout << "\nEnter the value of n:        ";
+    cin >> n;
+    while (n > INT_MAX || n < 0)
+    {
+        cout << "The inputted value of n is invalid. Please re-enter the value of n." << endl;
+        cout << "\nEnter the value of n:        ";
+        cin >> n;
+    }
+    cout << "\nThe result of S(" << n << ") is:      " << sum(n) << "." << endl;
+    return 0;
+}
 
-	return 0;
+int sum(int n)
+{
+    if (n == 0)
+        return 0;
+    return (sum(n - 1) + pow(n,4));
 }
