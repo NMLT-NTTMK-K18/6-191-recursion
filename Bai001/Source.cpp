065181408c86f3d815b1489eb834cbd71e9b4d8d
KevinNitroG <kevinnitro@duck.com>
2023-11-13 12:24:20 +0000

    💥 (Bai001): Done

--- Bai001/Source.cpp
+++ Bai001/Source.cpp
@@ -1,8 +1,24 @@
-#include <iostream>
+#include <iostream>;
+
 using namespace std;
+
+int Tong(int);
+
+int Tong(int n)
+{
+	if (n == 0)
+		return 0;
+	int s = Tong(n - 1);
+	return (s + n * n);
+}
 
 int main()
 {
+	int k;
+	cout << "Nhap n: ";
+	cin >> k;
 
-	return 0;
+	int kq = Tong(k);
+	cout << "S(" << k << ")= " << kq;
+	return 1;
 }
