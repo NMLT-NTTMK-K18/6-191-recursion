4fbbbe03905ebdf62bed07f8cd1010e90aa35fad
khiemhoangtruongUIT <137704231+truonghoangkhiem@users.noreply.github.com>
2023-11-14 11:05:02 +0700

    062-082 completed

--- Bai062/Source.cpp
+++ Bai062/Source.cpp
@@ -1,26 +1,22 @@
 ﻿#include<stdlib.h>
 #include<stdio.h>
 
-// Gộp hai mảng con arr[l...m] và arr[m+1..r]
 void merge(int arr[], int l, int m, int r)
 {
     int i, j, k;
     int n1 = m - l + 1;
     int n2 = r - m;
 
-    /* Tạo các mảng tạm */
     int L[1000], R[1000];
 
-    /* Copy dữ liệu sang các mảng tạm */
     for (i = 0; i < n1; i++)
         L[i] = arr[l + i];
     for (j = 0; j < n2; j++)
         R[j] = arr[m + 1 + j];
 
-    /* Gộp hai mảng tạm vừa rồi vào mảng arr*/
-    i = 0; // Khởi tạo chỉ số bắt đầu của mảng con đầu tiên
-    j = 0; // Khởi tạo chỉ số bắt đầu của mảng con thứ hai
-    k = l; // IKhởi tạo chỉ số bắt đầu của mảng lưu kết quả
+    i = 0; 
+    j = 0; 
+    k = l; 
     while (i < n1 && j < n2)
     {
         if (L[i] <= R[j])
@@ -36,7 +32,6 @@ void merge(int arr[], int l, int m, int r)
         k++;
     }
 
-    /* Copy các phần tử còn lại của mảng L vào arr nếu có */
     while (i < n1)
     {
         arr[k] = L[i];
@@ -44,7 +39,6 @@ void merge(int arr[], int l, int m, int r)
         k++;
     }
 
-    /* Copy các phần tử còn lại của mảng R vào arr nếu có */
     while (j < n2)
     {
         arr[k] = R[j];
@@ -53,15 +47,12 @@ void merge(int arr[], int l, int m, int r)
     }
 }
 
-/* l là chỉ số trái và r là chỉ số phải của mảng cần được sắp xếp */
 void mergeSort(int arr[], int l, int r)
 {
     if (l < r)
     {
-        // Tương tự (l+r)/2, nhưng cách này tránh tràn số khi l và r lớn
         int m = l + (r - l) / 2;
 
-        // Gọi hàm đệ quy tiếp tục chia đôi từng nửa mảng
         mergeSort(arr, l, m);
         mergeSort(arr, m + 1, r);
 
@@ -70,7 +61,6 @@ void mergeSort(int arr[], int l, int r)
 }
 
 
-/* Hàm xuất mảng */
 void printArray(int A[], int size)
 {
     int i;
--- Bai077/Source.cpp
+++ Bai077/Source.cpp
@@ -1,8 +1,41 @@
 #include <iostream>
+#include <cstdlib>
+#include <iomanip>
+
 using namespace std;
+
+void Nhap(float[], int&);
+void LietKe(float[], int);
 
 int main()
 {
-
+	float a[100];
+	int n;
+	Nhap(a, n);
+	LietKe(a, n);
 	return 0;
+}
+
+void Nhap(float a[], int& n)
+{
+	cout << "Nhap n: ";
+	cin >> n;
+	cout << "Nhap mang: ";
+	srand(time(NULL));
+	for (int i = 0; i < n; i++)
+		a[i] = rand() % 201 - 100.00;
+	cout << "\nMang ban dau la: ";
+	for (int i = 0; i < n; i++)
+		cout << setw(5) << a[i];
+	cout << endl;
+	cout << "Cac vi tri am trong mang la: ";
+}
+
+void LietKe(float a[], int n)
+{
+	if (n == 0)
+		return;
+	LietKe(a, n - 1);
+	if (a[n - 1] < 0)
+		cout << setw(10)  << n-1;
 }
