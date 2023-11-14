2593fa9471a31cffc6ee317278ec4fa4bf5dffe0
MinhHuy1507 <ngominhhuy150705@gmail.com>
2023-11-14 22:59:12 +0700

    :sparkles: 014-049 Done

--- Bai049/Source.cpp
+++ Bai049/Source.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 using namespace std;
+int Tinh(int);
 
 int main()
 {
+	int n;
+	cout << "Nhap n: ";
+	cin >> n;
 
+	cout << "an = " << Tinh(n);
 	return 0;
+}
+
+int Tinh(int n)
+{
+	if (n == 0)
+		return 1;
+	if (n == 1)
+		return 2;
+	return Tinh(n - 1) * 4 + Tinh(n - 2);
 }
