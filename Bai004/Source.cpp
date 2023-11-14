0ff1c33577114ba4b31d2fcb82453df2dc9d37a8
MinhHuy1507 <ngominhhuy150705@gmail.com>
2023-11-14 18:53:54 +0700

    :sparkles: 004-009 Done

--- Bai004/Source.cpp
+++ Bai004/Source.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 using namespace std;
+float Tong(int);
 
 int main()
 {
-
+	int n;
+	cout << "Nhap n: ";
+	cin >> n;
+	cout << "Tong S: " << Tong(n);
 	return 0;
+}
+
+float Tong(int n)
+{
+	if (n == 0)
+		return 1;
+	float s = Tong(n - 1);
+	return (s + (float)1 / (2 * n + 1));
 }
--- Bai009/Source.cpp
+++ Bai009/Source.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
 using namespace std;
+double LuyThua(double, int);
 
 int main()
 {
+	double x;
+	cout << "Nhap x: ";
+	cin >> x;
+
+	int n;
+	cout << "Nhap n: ";
+	cin >> n;
+
+	cout << "x^n = " << LuyThua(x, n);
 
 	return 0;
+}
+
+double LuyThua(double x, int n)
+{
+	if (n == 0)
+		return x;
+	double s = LuyThua(x, n - 1);
+	return s * x;
 }
