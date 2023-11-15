7132877f765c3277aba28a342bc3db5b8a716cae
khiemhoangtruongUIT <137704231+truonghoangkhiem@users.noreply.github.com>
2023-11-15 23:25:33 +0700

    122 127 completed

--- Bai122/Source.cpp
+++ Bai122/Source.cpp
@@ -1,8 +1,37 @@
-#include <iostream>
+#include<iostream>
+#include<iomanip>
 using namespace std;
+int DauTien(int[], int);
+void Nhap(int a[], int& n);
 
 int main()
 {
-
+	int a[100];
+	int n;
+	Nhap(a, n);
+	int kq = DauTien(a, n);
+	cout << "Gia tri le dau tien la: " << kq;
 	return 0;
 }
+int DauTien(int a[], int n)
+{
+	if (n == 0)
+		return 0;
+	int s = DauTien(a, n - 1);
+	if (s != 0)
+		return s;
+	if (a[n - 1] % 2 != 0)
+		return a[n - 1];
+	return 0;
+}
+
+void Nhap(int a[], int& n)
+{
+	cout << "Nhap n:";
+	cin >> n;
+	cout << "Nhap mang: ";
+	cout << endl;
+	for (int i = 0; i < n; i++)
+		cin >> a[i];
+}
+
--- Bai127/Source.cpp
+++ Bai127/Source.cpp
@@ -1,8 +1,50 @@
-#include <iostream>
+#include<iostream>
+#include<iomanip>
 using namespace std;
+int ViTriCuoi(int[], int);
+void Nhap(int a[], int& n);
+bool ktHoanThien(int);
 
 int main()
 {
-
+	int a[100];
+	int n;
+	Nhap(a, n);
+	int kq = ViTriCuoi(a, n);
+	cout << "Vi tri so hoan thien cuoi cung la: " << kq;
 	return 0;
 }
+int ViTriCuoi(int a[], int n)
+{
+	if (n == 0)
+		return -1;
+	if (ktHoanThien(a[n - 1]))
+		return n - 1;
+	return (ViTriCuoi(a, n - 1));
+}
+
+void Nhap(int a[], int& n)
+{
+	cout << "Nhap n:";
+	cin >> n;
+	cout << "Nhap mang: ";
+	cout << endl;
+	for (int i = 0; i < n; i++)
+		cin >> a[i];
+}
+
+bool ktHoanThien(int a)
+{
+	int s = 0;
+	int n = 1;
+	while (n < a)
+	{
+		if (a % n == 0)
+			s += n;
+		n++;
+	}
+	if (a == s)
+		return true;
+	return false;
+}
+
