611509360173a44e495628253439530872ef8933
Tuệ Dương Thông <23521734@gm.uit.edu.vn>
2023-11-16 22:33:02 +0700

    Bai050. Completed

--- Bai050/Source.cpp
+++ Bai050/Source.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
 using namespace std;
 
+int Tinh(int);
+
 int main()
 {
-
+	int n;
+	cout << "Nhap n: ";
+	cin >> n;
+	cout << "An = " << Tinh(n);
 	return 0;
+}
+
+int Tinh(int n)
+{
+	if (n == 0)
+		return -1;
+	if (n == 1)
+		return 3;
+	int x = Tinh(n - 1);
+	int y = Tinh(n - 2);
+	return 5 * x - y;
 }
