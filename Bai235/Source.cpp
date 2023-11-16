b31e2525847e7638c427c504e93e6dc4c62c293f
Tuệ Dương Thông <23521734@gm.uit.edu.vn>
2023-11-17 01:16:08 +0700

    Bai235. Completed

--- Bai235/Source.cpp
+++ Bai235/Source.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
 using namespace std;
 
+void Nhap(float[][100], int&, int&);
+int DemAm(float[][100], int, int, int);
+
 int main()
 {
-
+	int m, n, c;
+	float a[100][100];
+	Nhap(a, m, n);
+	cout << "Nhap c: ";
+	cin >> c;
+	cout << DemAm(a, m, n, c);
 	return 0;
+}
+
+void Nhap(float a[][100], int& m, int& n)
+{
+	cout << "Nhap m: ";
+	cin >> m;
+	cout << "Nhap n: ";
+	cin >> n;
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+			cin >> a[i][j];
+}
+
+int DemAm(float a[][100], int m, int n, int c)
+{
+	if (m == 0)
+		return 0;
+	int dem = DemAm(a, m - 1, n, c);
+	if (a[m - 1][c] < 0)
+		dem = dem + 1;
+	return dem;
 }
