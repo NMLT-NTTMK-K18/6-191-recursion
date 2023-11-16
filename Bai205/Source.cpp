b52b47b5b1a092e155c4d1bcdc879c2ecdad71f9
Tuệ Dương Thông <23521734@gm.uit.edu.vn>
2023-11-17 01:33:01 +0700

    Bai205. Completed

--- Bai205/Source.cpp
+++ Bai205/Source.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
 using namespace std;
 
+void Nhap(int[][100], int&, int&);
+float TichLe(int[][100], int, int);
+
 int main()
 {
-
+	int m, n;
+	int a[100][100];
+	Nhap(a, m, n);
+	cout << TichLe(a, m, n);
 	return 0;
+}
+
+void Nhap(int a[][100], int& m, int& n)
+{
+	cout << "Nhap m: ";
+	cin >> m;
+	cout << "Nhap n: ";
+	cin >> n;
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+			cin >> a[i][j];
+}
+
+float TichLe(int a[][100], int m, int n)
+{
+	if (m == 0)
+		return 1;
+	int T = TichLe(a, m - 1, n);
+	for (int j = 0; j < n; j++)
+		if (a[m - 1][j] % 2 != 0 && (m - 1) % 2 == 0)
+			T = T * a[m - 1][j];
+	return T;
 }
