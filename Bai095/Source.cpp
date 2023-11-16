e22e09d6955030e25f4b3bdb68ed430abcf7d44e
Tuệ Dương Thông <23521734@gm.uit.edu.vn>
2023-11-16 23:13:56 +0700

    Bai095. Completed

--- Bai095/Source.cpp
+++ Bai095/Source.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
 using namespace std;
 
+int DemChan(int[], int);
+
 int main()
 {
-
+	int n;
+	int a[100];
+	cout << "Nhap n: ";
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		cin >> a[i];
+	cout << "Co " << DemChan(a, n) << " so chan";
 	return 0;
+}
+
+int DemChan(int a[], int n)
+{
+	if (n == 0)
+		return 0;
+	int dem = DemChan(a, n - 1);
+	if (a[n - 1] % 2 == 0)
+		dem++;
+	return dem;
 }
