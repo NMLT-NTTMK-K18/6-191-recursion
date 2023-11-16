2d1107abf77ddd3ef784b5db137809d4ea45e68c
Tuệ Dương Thông <23521734@gm.uit.edu.vn>
2023-11-16 23:43:53 +0700

    Bai145. Completed

--- Bai145/Source.cpp
+++ Bai145/Source.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
 using namespace std;
 
+int ktToanChan(int[], int);
+
 int main()
 {
+	int n;
+	int a[100];
+	cout << "Nhap n: ";
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		cin >> a[i];
+	cout << ktToanChan(a, n);
+	return 0;
+}
 
+int ktToanChan(int a[], int n)
+{
+	if (n == 0)
+		return 0;
+	if (n == 1)
+	{
+		if (a[n - 1] % 2 == 0)
+			return 1;
+		return 0;
+	}
+	if (a[n - 1] % 2 == 0 && ktToanChan(a, n - 1) == 1)
+		return 1;
 	return 0;
 }
