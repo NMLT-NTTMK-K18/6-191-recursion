b50ae8149151535367699679cf5f2669941ea4f7
Tuệ Dương Thông <23521734@gm.uit.edu.vn>
2023-11-16 22:16:49 +0700

    Bai020. Completed

--- Bai020/Source.cpp
+++ Bai020/Source.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
+
+float Tong(int);
 
 int main()
 {
-
+	float n;
+	cout << "Nhap n: ";
+	cin >> n;
+	cout << "S = " << Tong(n);
 	return 0;
+}
+
+float Tong(int n)
+{
+	if (n == 0)
+		return 0;
+	float s = Tong(n - 1);
+	return s + sqrt(1 + (float)1 / n / n + (float)1 / (n + 1) / (n + 1));
 }
