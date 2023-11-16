1d4f44f9091415ff3b6967def01a6d1ff37aa550
Tuệ Dương Thông <23521734@gm.uit.edu.vn>
2023-11-17 00:38:31 +0700

    Bai290. Completed

--- Bai290/Source.cpp
+++ Bai290/Source.cpp
@@ -1,8 +1,54 @@
 #include <iostream>
 using namespace std;
 
+void Nhap(float[][100], int&, int&);
+void Xuat(float[][100], int, int);
+void DichPhaiDong(float[][100], int, int, int);
+void DichPhai(float[][100], int, int);
+
 int main()
 {
-
+	int m, n;
+	float a[100][100];
+	Nhap(a, m, n);
+	DichPhai(a, m, n);
+	Xuat(a, m, n);
 	return 0;
+}
+
+void Nhap(float a[][100], int& m, int& n)
+{
+	cout << "Nhap m: ";
+	cin >> m;
+	cout << "Nhap n: ";
+	cin >> n;
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+			cin >> a[i][j];
+}
+
+void Xuat(float a[][100], int m, int n)
+{
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+			cout << a[i][j] << " ";
+		cout << endl;
+	}
+}
+
+void DichPhaiDong(float a[][100], int m, int n, int d)
+{
+	float temp = a[d][n - 1];
+	for (int j = n - 1; j >= 1; j--)
+		a[d][j] = a[d][j - 1];
+	a[d][0] = temp;
+}
+
+void DichPhai(float a[][100], int m, int n)
+{
+	if (m == 0)
+		return;
+	DichPhai(a, m - 1, n);
+	DichPhaiDong(a, m, n, m - 1);
 }
