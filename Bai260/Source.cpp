fd543b17f1584dd4d2dd734650c9df91e72f7d56
Tuệ Dương Thông <23521734@gm.uit.edu.vn>
2023-11-17 01:08:23 +0700

    Bai260. Completed

--- Bai260/Source.cpp
+++ Bai260/Source.cpp
@@ -1,8 +1,45 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
+
+void Nhap(float[][100], int&, int&);
+int ktDong(float[][100], int, int, int);
+void LietKe(float[][100], int, int);
 
 int main()
 {
-
+	int m, n;
+	float a[100][100];
+	Nhap(a, m, n);
+	LietKe(a, m, n);
 	return 0;
+}
+
+void Nhap(float a[][100], int& m, int& n)
+{
+	cout << "Nhap m: ";
+	cin >> m;
+	cout << "Nhap n: ";
+	cin >> n;
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+			cin >> a[i][j];
+}
+
+int ktDong(float a[][100], int m, int n, int d)
+{
+	if (n == 0)
+		return 0;
+	if (a[d][n - 1] < 0)
+		return 1;
+	return ktDong(a, m, n - 1, d);
+}
+
+void LietKe(float a[][100], int m, int n)
+{
+	if (m == 0)
+		return;
+	LietKe(a, m - 1, n);
+	if (ktDong(a, m, n, m - 1) == 1)
+		cout << setw(4) << (m - 1);
 }
