e49d284bff3e229c571fe6f2891e449c0dc10779
Tuệ Dương Thông <23521734@gm.uit.edu.vn>
2023-11-17 01:31:54 +0700

    Bai210. Completed

--- Bai210/Source.cpp
+++ Bai210/Source.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
 using namespace std;
 
+void Nhap(float[][100], int&, int&);
+float TongDuong(float[][100], int, int);
+
 int main()
 {
-
+	int m, n;
+	float a[100][100];
+	Nhap(a, m, n);
+	cout << TongDuong(a, m, n);
 	return 0;
+}
+
+void Nhap(float a[][100], int& m, int& n)
+{
+	cout << "Nhap m: ";
+	cin >> m;
+	cout << "Nhap n: ";
+	cin >> n;
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+			cin >> a[i][j];
+}
+
+float TongDuong(float a[][100], int m, int n)
+{
+	if (m == 0)
+		return 0;
+	float s = TongDuong(a, m - 1, n);
+	for (int j = 0; j < n; j++)
+		if (a[m - 1][j] > 0)
+			s = s + a[m - 1][j];
+	return s;
 }
