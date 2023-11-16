f487aa049ab3224df146499ac95e47cb67e3c2d2
Tuệ Dương Thông <23521734@gm.uit.edu.vn>
2023-11-17 01:40:42 +0700

    Bai185. Completed

--- Bai185/Source.cpp
+++ Bai185/Source.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
 using namespace std;
 
+void Nhap(float[], int&);
+int DemPhanBiet(float[], int);
+
 int main()
 {
-
+	int n;
+	float a[100];
+	Nhap(a, n);
+	cout << DemPhanBiet(a, n);
 	return 0;
+}
+
+void Nhap(float a[], int& n)
+{
+	cout << "Nhap n: ";
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		cin >> a[i];
+}
+
+int DemPhanBiet(float a[], int n)
+{
+	if (n == 0)
+		return 0;
+	if (n == 1)
+		return 1;
+	int dem = DemPhanBiet(a, n - 1);
+
+	int flag = 1;
+	for (int i = 0; i <= n - 2; i++)
+		if (a[i] == a[n - 1])
+			flag = 0;
+	if (flag == 1)
+		dem++;
+	return dem;
 }
