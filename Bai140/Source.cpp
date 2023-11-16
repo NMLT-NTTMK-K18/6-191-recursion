4c10029f859f5a98ccc2912d818ea6c4aa7b196b
Tuệ Dương Thông <23521734@gm.uit.edu.vn>
2023-11-16 23:42:37 +0700

    Bai140. Completed

--- Bai140/Source.cpp
+++ Bai140/Source.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 using namespace std;
 
+int ktKhong(int[], int);
+
 int main()
 {
-
+	int n;
+	int a[100];
+	cout << "Nhap n: ";
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		cin >> a[i];
+	cout << ktKhong(a, n);
 	return 0;
+}
+
+int ktKhong(int a[], int n)
+{
+	if (n == 0)
+		return 0;
+	if (a[n - 1] == 0)
+		return 1;
+	return ktKhong(a, n - 1);
 }
