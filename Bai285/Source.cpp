3d50458aa420c50c88d8045db23119bd8c79a50a
Tuệ Dương Thông <23521734@gm.uit.edu.vn>
2023-11-17 00:41:49 +0700

    Bai285. Completed

--- Bai285/Source.cpp
+++ Bai285/Source.cpp
@@ -1,8 +1,47 @@
 #include <iostream>
 using namespace std;
 
+void Nhap(float[][100], int&, int&);
+void Xuat(float[][100], int, int);
+void BienDoi(float[][100], int, int);
+
 int main()
 {
-
+	int m, n;
+	float a[100][100];
+	Nhap(a, m, n);
+	BienDoi(a, m, n);
+	Xuat(a, m, n);
 	return 0;
+}
+
+void Nhap(float a[][100], int& m, int& n)
+{
+	cout << "Nhap m: ";
+	cin >> m;
+	cout << "Nhap n: ";
+	cin >> n;
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+			cin >> a[i][j];
+}
+
+void Xuat(float a[][100], int m, int n)
+{
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+			cout << a[i][j] << " ";
+		cout << endl;
+	}
+}
+
+void BienDoi(float a[][100], int m, int n)
+{
+	if (m == 0)
+		return;
+	BienDoi(a, m - 1, n);
+	for (int j = 0; j < n; j++)
+		if (a[m - 1][j] < 0)
+			a[m - 1][j] = -a[m - 1][j];
 }
