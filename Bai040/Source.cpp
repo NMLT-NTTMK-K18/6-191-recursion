728cd7f278ed6c39de9fb2f95674885566c92b68
Tuệ Dương Thông <23521734@gm.uit.edu.vn>
2023-11-16 22:27:18 +0700

    Bai040. Completed

--- Bai040/Source.cpp
+++ Bai040/Source.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
+
+int UocLeLonNhat(int);
 
 int main()
 {
-
+	int n;
+	cout << "Nhap n: ";
+	cin >> n;
+	cout << "Uoc le lon nhat = " << UocLeLonNhat(n);
 	return 0;
+}
+
+int UocLeLonNhat(int n)
+{
+	n = abs(n);
+	if (n % 2 != 0)
+		return n;
+	return UocLeLonNhat(n / 2);
 }
