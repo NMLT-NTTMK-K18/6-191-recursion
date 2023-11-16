46b372268359500a20e55712b3e86e0b1e20489d
Tuệ Dương Thông <23521734@gm.uit.edu.vn>
2023-11-16 23:24:19 +0700

    Bai110. Completed

--- Bai110/Source.cpp
+++ Bai110/Source.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
+
+float DuongDau(float[], int);
 
 int main()
 {
+	int n;
+	float a[100];
+	cout << "Nhap n: ";
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		cin >> a[i];
+	cout << "Duong dau: " << DuongDau(a, n);
+	return 0;
+}
 
+float DuongDau(float a[], int n)
+{
+	if (n == 0)
+		return 0;
+	float lc = DuongDau(a, n - 1);
+	if (lc != 0)
+		return lc;
+	if (a[n - 1] > 0)
+		return a[n - 1];
 	return 0;
 }
