ea054e6e1e08c1719e4e53785399e518fd3dff7d
Tuệ Dương Thông <23521734@gm.uit.edu.vn>
2023-11-17 01:08:45 +0700

    Bai255. Completed

--- Bai255/Source.cpp
+++ Bai255/Source.cpp
@@ -1,8 +1,44 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
+
+void Nhap(float[][100], int&, int&);
+int ktToanDuong(float[][100], int, int);
 
 int main()
 {
-
+	int m, n;
+	float a[100][100];
+	Nhap(a, m, n);
+	cout << ktToanDuong(a, m, n);
 	return 0;
+}
+
+void Nhap(float a[][100], int& m, int& n)
+{
+	cout << "Nhap m: ";
+	cin >> m;
+	cout << "Nhap n: ";
+	cin >> n;
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+			cin >> a[i][j];
+}
+
+int ktToanDuong(float a[][100], int m, int n)
+{
+	if (m == 0)
+		return 0;
+	if (m == 1)
+	{
+		int flag = 1;
+		for (int j = 0; j < n; j++)
+			if (a[0][j] <= 0)
+				flag = 0;
+		return flag;
+	}
+	for (int j = 0; j < n; j++)
+		if (a[m - 1][j] <= 0)
+			return 0;
+	return ktToanDuong(a, m - 1, n);
 }
