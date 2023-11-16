9b0112982c9072bc4644c0da8042b5943648e264
Tuệ Dương Thông <23521734@gm.uit.edu.vn>
2023-11-16 22:20:20 +0700

    Bai025. Completed

--- Bai025/Source.cpp
+++ Bai025/Source.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
+
+int TichSoLe(int);
 
 int main()
 {
-
+	int n;
+	cout << "Nhap n: ";
+	cin >> n;
+	cout << "T = " << TichSoLe(n);
 	return 0;
+}
+
+int TichSoLe(int n)
+{
+	n = abs(n);
+	if (n <= 9)
+	{
+		if (n % 2 != 0)
+			return n;
+		return 0;
+	}
+	int t = TichSoLe(n / 10);
+	int dv = n % 10;
+	if (dv % 2 != 0)
+		return t * dv;
+	return t;
 }
