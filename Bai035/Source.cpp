733e883fb4962c3494668eca0d162e5f633a489b
Tuệ Dương Thông <23521734@gm.uit.edu.vn>
2023-11-16 22:24:53 +0700

    Bai035. Completed

--- Bai035/Source.cpp
+++ Bai035/Source.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
+
+void LietKe(int);
 
 int main()
 {
-
+	int n;
+	cout << "Nhap n: ";
+	cin >> n;
+	LietKe(n);
 	return 0;
+}
+
+void LietKe(int n)
+{
+	if (n == 1)
+	{
+		cout << setw(10) << n;
+		return;
+	}
+	cout << setw(10) << n;
+	if (n % 2 == 0)
+		LietKe(n / 2);
+	else
+		LietKe(3 * n + 1);
 }
