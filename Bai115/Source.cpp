22b456d0b4ab3abe739acb54d87f9689bd34ee20
Tuệ Dương Thông <23521734@gm.uit.edu.vn>
2023-11-16 23:26:07 +0700

    Bai115. Completed

--- Bai115/Source.cpp
+++ Bai115/Source.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 using namespace std;
 
+int ChanCuoi(int[], int);
+
 int main()
 {
-
+	int n;
+	int a[100];
+	cout << "Nhap n: ";
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		cin >> a[i];
+	cout << "Chan cuoi: " << ChanCuoi(a, n);
 	return 0;
+}
+
+int ChanCuoi(int a[], int n)
+{
+	if (n == 0)
+		return -1;
+	if (a[n - 1] % 2 == 0)
+		return a[n - 1];
+	return ChanCuoi(a, n - 1);
 }
