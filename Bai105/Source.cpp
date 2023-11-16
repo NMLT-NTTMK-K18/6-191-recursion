8fe9d44734e501fd0f1256255ddbe09e0ff5d669
Tuệ Dương Thông <23521734@gm.uit.edu.vn>
2023-11-16 23:19:37 +0700

    Bai105. Completed

--- Bai105/Source.cpp
+++ Bai105/Source.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
+
+int TimViTri(float[], int);
 
 int main()
 {
-
+	int n;
+	float a[100];
+	cout << "Nhap n: ";
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		cin >> a[i];
+	cout << "Vi tri = " << TimViTri(a, n);
 	return 0;
+}
+int TimViTri(float a[], int n)
+{
+	if (n == 1)
+		return 0;
+	int lc = TimViTri(a, n - 1);
+	if (a[n - 1] < a[lc])
+		lc = n - 1;
+	return lc;
 }
