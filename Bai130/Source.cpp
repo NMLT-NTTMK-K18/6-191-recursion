ec760e483f5eaed8879aff18526b03fb02e291aa
Tuệ Dương Thông <23521734@gm.uit.edu.vn>
2023-11-16 23:37:00 +0700

    Bai130. Completed

--- Bai130/Source.cpp
+++ Bai130/Source.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
 using namespace std;
 
+float AmLonNhat(float[], int);
+
 int main()
 {
-
+	int n;
+	float a[100];
+	cout << "Nhap n: ";
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		cin >> a[i];
+	cout << "Am lon nhat: " << AmLonNhat(a, n);
 	return 0;
+}
+
+float AmLonNhat(float a[], int n)
+{
+	if (n == 0)
+		return 0;
+	float lc = AmLonNhat(a, n - 1);
+	if (a[n - 1] >= 0)
+		return lc;
+	if (lc == 0)
+		return a[n - 1];
+	if (a[n - 1] > lc)
+		lc = a[n - 1];
+	return lc;
 }
