c3a130574d6ab9a1afb984468c87ce85ea2aca0b
khiemhoangtruongUIT <137704231+truonghoangkhiem@users.noreply.github.com>
2023-11-17 23:31:23 +0700

    287 finish

--- Bai287/Source.cpp
+++ Bai287/Source.cpp
@@ -1,8 +1,60 @@
 #include <iostream>
+#include <iomanip>
+
 using namespace std;
+
+void Nhap(float[][100], int&, int&);
+void Xuat(float[][100], int, int);
+void DichXuongCot(float[][100], int, int);
+void DichXuong(float[][100], int, int);
 
 int main()
 {
-
+	float a[100][100];
+	int m, n;
+	Nhap(a, m, n);
+	DichXuong(a, m, n);
+	Xuat(a, m, n);
 	return 0;
+}
+
+void Nhap(float a[][100], int& m, int& n)
+{
+	cout << "Nhap so dong: ";
+	cin >> m;
+	cout << "Nhap so cot: ";
+	cin >> n;
+	for (int i = 0; i < m; ++i)
+		for (int j = 0; j < n; ++j)
+		{
+			cout << "Nhap a[" << i << "][" << j << "]: ";
+			cin >> a[i][j];
+		}
+}
+
+void Xuat(float a[][100], int m, int n)
+{
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+			cout << a[i][j] << " ";
+		cout << endl;
+	}
+}
+
+void DichXuongCot(float a[][100],
+	int m, int n, int c)
+{
+	float temp = a[m-1][c];
+	for (int i=m-1; i >=1; i--)
+		a[i][c] = a[i - 1][c];
+	a[0][c] = temp;
+}
+
+void DichXuong(float a[][100], int m, int n)
+{
+	if (n == 0)
+		return;
+	DichXuong(a, m, n - 1);
+	DichXuongCot(a, m, n, n - 1);
 }
